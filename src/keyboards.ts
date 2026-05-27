715706690aa7467abf1d9d0735ba94020fb3d68e
visatk <visatk29@gmail.com>
2026-05-27 18:44:49 +0600

    Update keyboards.ts

--- src/keyboards.ts
+++ src/keyboards.ts
@@ -1,1 +1,14 @@
+import { Keyboard, InlineKeyboard } from 'grammy';
 
+export const continueKeyboard = new InlineKeyboard()
+	.text('✅ Continue', 'action_continue');
+
+export const cancelKeyboard = new Keyboard()
+	.text('❌ Cancel')
+	.resized()
+	.placeholder('Cancel current action...');
+
+export const mainKeyboard = new Keyboard()
+	.text('💰 Balance').text('👥 Referral').row()
+	.text('📤 Withdraw').text('🔄 How It Works')
+	.resized();
